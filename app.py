bb9d22ad1528e1ae914887a45225119219594d8b
lipemont <162990243+lipemont@users.noreply.github.com>
2026-05-19 00:22:48 -0300

    corrigindo porta render

--- app.py
+++ app.py
@@ -191,4 +191,6 @@ def add_atendimento():
     return jsonify({"mensagem": "Atendimento agendado com sucesso!", "atendimento": novo_atendimento}), 201
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+    porta = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=porta)
